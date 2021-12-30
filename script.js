4f0036b8a1575168442e7c09d3711aadcc88d6a7
Mason Koenig <koenig.mason@gmail.com>
2021-12-30 17:49:13 -0500

    Added grid, formatting, and controls

--- script.js
+++ script.js
@@ -4,14 +4,60 @@ function setAttributes(el, attrs) {
   }
 }
 
-const container = document.createElement('div');
-container.id = 'container';
-document.body.append(container);
-
-for(let i = 0; i < 16; i++) {
-  let div = document.createElement('div');
-  div.id = `cell${i}`
-  container.append(div);
+function nextChar(startChar, increment) {
+  return String.fromCharCode(startChar.charCodeAt(0) + increment);
 }
 
-console.log(container)
+function makeGrid(numUnits) {
+  let startChar = 65;
+  for(let i = 0; i < numUnits; i++) {
+    let row = document.createElement('div');
+    row.className = `row`
+    container.append(row);
+    for(let i = 1; i <= numUnits; i++) {
+      let div = document.createElement('div');
+      div.className = `box`
+      row.append(div);
+    }
+    startChar++
+  }    
+}
+
+function removeChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function hoverEffect() {
+  const box = document.querySelectorAll('.box');
+  box.forEach(e => {
+    e.addEventListener('mouseenter', () => {
+      e.style.backgroundColor = 'grey';
+  })
+})
+}
+
+//Create Container to contain grid
+const container = document.createElement('div');
+container.className = 'container';
+document.body.append(container);
+
+//Define Grid size, create
+let gridSize = 100;
+makeGrid(gridSize);
+hoverEffect();
+
+//Controls
+const resetBtn = document.createElement('button');
+resetBtn.textContent = 'Reset';
+resetBtn.style.margin = '10px';
+resetBtn.style.paddingInline = '10px';
+container.parentNode.insertBefore(resetBtn, container);
+resetBtn.addEventListener('click', () => {
+  removeChildNodes(container);
+  gridSize = window.prompt('Enter grid size.', 16);
+  makeGrid(gridSize);
+  hoverEffect();
+})
+
